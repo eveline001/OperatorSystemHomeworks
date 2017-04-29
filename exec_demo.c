02e4fe75bbe0e925f66e3ea6c403d4a4c499ad6e
EvilGiven <evilgiven@gmail.com>
2017-04-29 19:00:29 +0800

    fixed

---
+++ exec_demo.c
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+	pid_t pid; 
+	
+	/* fork a child process */
+	pid = fork();
+
+	if(pid < 0){ 
+
+	/* error occurred */
+		fprintf(stderr, "Fork Failed");
+		return 1;
+
+	}else if(pid == 0){
+
+	/* 子进程 */
+		execlp("/bin/ls","ls",NULL);
+
+	}else{
+
+	/* 父进程 */
+
+	/* 父进程将一直等到，直到子进程运行完毕*/
+		wait(NULL);
+		printf("Child Complete");
+
+	}
+
+	return 0;
+} 
