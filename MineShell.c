d0d3de64ed924fbc808883278d2a51c103c6a1b2
EvilGiven <evilgiven@gmail.com>
2017-06-15 21:21:59 +0800

    fixed

--- MineShell.c
+++ MineShell.c
@@ -19,35 +19,31 @@ int main()
         if(strcmp(op, "quit") == 0 || strcmp(op, "exit") == 0 || strcmp(op, "bye") == 0){
             printf("\nQuiting...Bye~");
             return 0;
-        }
-        if(strcmp(op, "help") == 0){
+        }else if(strcmp(op, "help") == 0){
             printf("It's a fake shell~\n");
-        }
-        if(strcmp(op, "cd") == 0){
+        }else if(strcmp(op, "cd") == 0){
             scanf("%s", arg);
             sprintf(str, "cd %s", arg);
             system(str);
-        }
-        if(strcmp(op, "environ") == 0){
+        }else if(strcmp(op, "environ") == 0){
             system("env");
-        }
-        if(strcmp(op, "echo") == 0){
+        }else if(strcmp(op, "echo") == 0){
             scanf("%s", arg);
             sprintf(str, "echo %s", arg);
             system(str);
-        }
-        if(strcmp(op, "jobs") == 0){
+        }else if(strcmp(op, "jobs") == 0){
             system("ps");
-        }
-        fgets(arg, 256, stdin);
-        sprintf(str, "%s %s", op, arg);
-        while((CurSon = fork()) < 0);
-        if(CurSon == 0){
-            system(str);
         }else{
-            wait(NULL);
-            printf("%s\n", str);
-            printf("Complete...\n");
+        	fgets(arg, 256, stdin);
+        	sprintf(str, "%s %s", op, arg);
+        	while((CurSon = fork()) < 0);
+        	if(CurSon == 0){
+            	system(str);
+        	}else{
+            	wait(NULL);
+            	printf("%s\n", str);
+            	printf("Complete...\n");
+        	}
         }
     }
     return 0;
